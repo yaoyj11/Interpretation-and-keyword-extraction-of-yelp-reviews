55b90688df817891047d15b8bd2e775ff62916bb
Yuanjun Yao <yy126@duke.edu>
2016-10-21 00:03:30 -0400

    init

---
+++ parsing.py
@@ -0,0 +1,47 @@
+from __future__ import print_function
+
+import re
+import sys
+from operator import add
+
+from pyspark.conf import SparkConf
+from pyspark.context import SparkContext
+
+
+
+if __name__ == "__main__":
+
+    inputdir="yelp_dataset"
+    conf = SparkConf()
+    conf.setMaster("local").setAppName("YELP")
+    sc = SparkContext
+
+    # Initialize the spark context.
+
+    # Loads in input file. It should be in format of:
+    #     URL         neighbor URL
+    #     URL         neighbor URL
+    #     URL         neighbor URL
+    #     ...
+    lines = spark.read.text(sys.argv[1]).rdd.map(lambda r: r[0])
+
+    # Loads all URLs from input file and initialize their neighbors.
+    links = lines.map(lambda urls: parseNeighbors(urls)).distinct().groupByKey().cache()
+
+    # Loads all URLs with other URL(s) link to from input file and initialize ranks of them to one.
+    ranks = links.map(lambda url_neighbors: (url_neighbors[0], 1.0))
+
+    # Calculates and updates URL ranks continuously using PageRank algorithm.
+    for iteration in range(int(sys.argv[2])):
+        # Calculates URL contributions to the rank of other URLs.
+        contribs = links.join(ranks).flatMap(
+            lambda url_urls_rank: computeContribs(url_urls_rank[1][0], url_urls_rank[1][1]))
+
+        # Re-calculates URL ranks based on neighbor contributions.
+        ranks = contribs.reduceByKey(add).mapValues(lambda rank: rank * 0.85 + 0.15)
+
+    # Collects all URL ranks and dump them to console.
+    for (link, rank) in ranks.collect():
+        print("%s has rank: %s." % (link, rank))
+
+    spark.stop()
